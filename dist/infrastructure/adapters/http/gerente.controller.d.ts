5e213ded84184638da41bc442a879c0aaebe9f23
Mayara Rocha <98711190+MayaraRocha95@users.noreply.github.com>
2024-08-05 16:00:58 -0300

    Revert "fix: atualizando os dados do gerente"

--- dist/infrastructure/adapters/http/gerente.controller.d.ts
+++ dist/infrastructure/adapters/http/gerente.controller.d.ts
@@ -1,10 +1,19 @@
 import { GerenteService } from '../../../domain/services/gerente.service';
 import { CreateGerenteDto } from '../../../presentation/dtos/create-gerente.dto';
-import { UpdateGerenteDto } from 'src/presentation/dtos/update-gerente.dto';
+import { CreateContaDto } from '../../../presentation/dtos/create-conta.dto';
 export declare class GerenteController {
     private readonly gerenteService;
     constructor(gerenteService: GerenteService);
     create(createGerenteDto: CreateGerenteDto): Promise<import("../../../domain/entities/gerente.entity").Gerente>;
     findAll(): Promise<import("../../../domain/entities/gerente.entity").Gerente[]>;
-    update(id: string, updateGerenteDto: UpdateGerenteDto): Promise<import("../../../domain/entities/gerente.entity").Gerente>;
+    addCliente(gerenteId: string, clienteId: string): Promise<import("../../../domain/entities/gerente.entity").Gerente>;
+    removeCliente(gerenteId: string, clienteId: string): Promise<{
+        success: boolean;
+        message: string;
+    }>;
+    modifyConta(gerenteId: string, contaId: string, { tipo }: {
+        tipo: 'corrente' | 'poupanca';
+    }): Promise<import("../../../domain/entities/conta.entity").Conta>;
+    openConta(gerenteId: string, createContaDto: CreateContaDto): Promise<import("../../../domain/entities/conta.entity").Conta>;
+    closeConta(gerenteId: string, contaId: string): Promise<boolean>;
 }
--- src/domain/entities/gerente.entity.ts
+++ src/domain/entities/gerente.entity.ts
@@ -4,14 +4,6 @@ import { Cliente } from './cliente.entity';
 export class Gerente {
   id: string;
   nome: string;
-  email: string;
-  senha: string;
-  telefone: string;
-  endereco: string;
-  dataInicio: Date;
-  status: 'ativo' | 'inativo';
-  departamento: string;
-  nivelAcesso: string;
   @Type(() => Cliente)
   clientes: Cliente[];
 }
