169a1c9bd27f331a38755de3814f7ae9ea8a43ba
Mayara Rocha <mayaraaraujorocha@hotmail.com>
2024-08-23 11:46:58 -0300

    fix: conflitos

--- src/infrastructure/modules/gerente.module.ts
+++ src/infrastructure/modules/gerente.module.ts
@@ -2,13 +2,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Gerente } from '../../domain/entities/gerente.entity';
+import { Conta } from '../../domain/entities/conta.entity';
+import { Credito } from '../../domain/entities/credito.entity';
 import { GerenteService } from '../../domain/services/gerente.service';
 import { GerenteController } from '../adapters/http/gerente.controller';
-import { Conta } from 'src/domain/entities/conta.entity';
-import { Credito } from 'src/domain/entities/credito.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Gerente, Conta, Credito])],
+  imports: [
+    TypeOrmModule.forFeature([Gerente, Conta, Credito]),
+  ],
   controllers: [GerenteController],
   providers: [GerenteService],
   exports: [GerenteService],
